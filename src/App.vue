085bc7510e617f44240a6aa0075472eec96fc4da
carlmenke <carlmenke13@gmail.com>
2022-10-17 10:28:54 -0700

    added sign in form and page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div class="form-container">
+      <form>
+        <input placeholder="Email" :value="email" name="email" type="email" />
+        <input
+          placeholder="Password"
+          :value="password"
+          name="password"
+          type="password"
+        />
+        <button>Log In</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import './styles/app.css'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  data: () => ({
+    email: '',
+    password: ''
+  }),
+  methods: {
+    handleFormChange() {},
+    handleSubmit() {}
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
